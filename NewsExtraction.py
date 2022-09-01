cb1430c4fff65e1bbfc7cce84429b6ca030f79ba
desmondharris <desmondktharris@gmail.com>
2022-09-01 13:04:12 -0500

    Add temporary(?) fix to extract_cnn method

--- NewsExtraction.py
+++ NewsExtraction.py
@@ -3,19 +3,31 @@ import requests
 
 
 def cnn_extract(link):
-    page = requests.get(link)
+    found = False
+    while not found:
+        page = requests.get(link)
+        if page is not None:
+            found = True
     soup = BeautifulSoup(page.text, "html.parser")
-    top_paragraph = soup.find('p',  class_="zn-body__paragraph speakable").getText()
-    body_html = soup.find_all('div', class_="zn-body__paragraph")
-    body_text = ""
+    # This was added because I ran into an error where seemingly randomly, I would get back a different soup object, and
+    # parsing it with the code in the try block would return None.
+    try:
+        body_text = soup.find('p', class_="zn-body__paragraph speakable").getText()
+        body_html = soup.find_all('p', class_="zn-body__paragraph" )
+    except AttributeError:
+        body_html = soup.select('.article__content p')
+        body_text = ""
     for i in body_html:
         body_text += i.getText()
-    top_paragraph += body_text
-    return top_paragraph
+    return body_text
 
 
 def fox_extract(link):
-    page = requests.get(link)
+    found = False
+    while not found:
+        page = requests.get(link)
+        if page is not None:
+            found = True
     soup = BeautifulSoup(page.text, "html.parser")
     paragraphs = soup.select('.article-body p')
     body_text = ""
@@ -25,7 +37,11 @@ def fox_extract(link):
 
 
 def cnbc_extract(link):
-    page = requests.get(link)
+    found = False
+    while not found:
+        page = requests.get(link)
+        if page is not None:
+            found = True
     soup = BeautifulSoup(page.text, "html.parser")
     body_html = soup.select('.group p')
     body_text = ""
@@ -35,7 +51,11 @@ def cnbc_extract(link):
 
 
 def nbc_extract(link):
-    page = requests.get(link)
+    found = False
+    while not found:
+        page = requests.get(link)
+        if page is not None:
+            found = True
     soup = BeautifulSoup(page.text, "html.parser")
     body_html = soup.select('.article-body__content p')
     body_text = ""
