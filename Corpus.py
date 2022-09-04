c643782ad484e68c4d67c55ba260245c240e20e6
desmondharris <desmondktharris@gmail.com>
2022-09-04 10:36:47 -0500

    Revamp IDF and TF calculations with one-time use dictionaries. Remove old vectorization methods to create new one in future. Add a few punctuation marks to stop words list.

--- Corpus.py
+++ Corpus.py
@@ -22,47 +22,30 @@ def code_lemmatize(words):
 # takes a preprocessed corpus as a list of documents NOT as object Corpus
 # returns a dictionary where the keys are each word in the corpus, and the values are a list containing which documents
 # use the term
-def dfdictionary(corpus):
+def idf_dictionary(corpus):
     dicti = {}
     for cnt, doc in enumerate(corpus):
         for word in doc:
-            # if the term is not in the dictionary, add it
             try:
                 dicti[word].add(cnt)
-            except:
+            except KeyError:
                 dicti[word] = {cnt}
-    return dicti
-
-
-# calculates idf using dfdictionary
-def idf(corpus, term):
-    dicti = dfdictionary(corpus)
-    return np.log(len(corpus) / (len(dicti[term])))
+    condensed_dict = {}
+    for key in dicti:
+        condensed_dict[key] = np.log(len(corpus)/len(dicti[key]))
+    return condensed_dict
 
 
 # takes a preproccesed document
 # returns a dictionary where the keys are each word in the given doc, and the values are the indices where the term is
-def tfdictionary(doc):
+def tf_dictionary(doc):
     dicti = {}
-    for cnt, word in enumerate(doc):
-        # if the term is not yet in the dictionary, add it
+    for word in doc:
         try:
-            dicti[word].add(cnt)
-        except:
-            dicti[word] = {cnt}
+            dicti[word] += 1
+        except KeyError:
+            dicti[word] = 1
     return dicti
-
-
-# takes a preproccesed document and
-# returns tf of a given document and term using tfdictionary
-def tf(doc, term):
-    dicti = tfdictionary(doc)
-    return len(dicti[term]) / len(doc)
-
-
-# calculates tf-idf
-def tfidf(corpus, doc, term):
-    return tf(doc, term) * idf(corpus, term)
 
 
 # generalized cosine similarity function for comparing two vectorized tf-df documents
@@ -74,6 +57,7 @@ class Corpus:
     def __init__(self, texts):
         self.docs = []
         self.vects = []
+        self.token_list = []
         self.docs = texts
         self.preprocess()
 
@@ -84,18 +68,10 @@ class Corpus:
     def preprocess(self):
         stopword = stopwords.words('english')
         # will get rid of punctuation in document as well
-        stopword.extend([',', '.', '``', "''", '--', '?', "n't", "'s", ':', '$', "'ve", "'d", "'", "-", '"', "'"])
+        stopword.extend([',', '.', '``', "''", '--', '?', "n't", "'s", ':', '$', "'ve", "'d", "'", "-", '"', "'", '’',
+                         '—', '“', '(', ')'])
         for cnt in range(len(self.docs)):
             self.docs[cnt] = self.docs[cnt].lower()
             self.docs[cnt] = nltk.word_tokenize(self.docs[cnt])
             self.docs[cnt] = [word for word in self.docs[cnt] if word not in stopword]
             self.docs[cnt] = code_lemmatize(self.docs[cnt])
-
-
-links = ["https://www.cnn.com/2022/08/31/tennis/serena-williams-anett-kontaveit-us-open-spt-intl/index.html"]
-articles = []
-for i in links:
-    articles.append(cnn(i))
-test = Corpus(articles)
-for i in test.docs:
-    print(i)
