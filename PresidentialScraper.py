2fd194d8c4909e4308249e3d29f64b10e38e28ba
Lance Hahn <41932220+LanceHahn@users.noreply.github.com>
2022-10-08 16:38:43 -0500

    working retraining; Documenting activity

--- PresidentialScraper.py
+++ PresidentialScraper.py
@@ -1,18 +1,23 @@
-import warnings
+
+import platform
+
+import urllib.request
+
 
 import nltk
-import numpy as np
+from nltk.corpus import stopwords
 from gensim.models import KeyedVectors, Word2Vec
 from gensim.test.utils import get_tmpfile
 from gensim.scripts.glove2word2vec import glove2word2vec as g2w
+from gensim.test.utils import datapath
 import urllib.request
 import re
 
-from nltk.corpus import stopwords
 
 from bs4 import BeautifulSoup
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+
 
 class PresidentialScraper:
     def __init__(self, link):
@@ -21,15 +26,28 @@ class PresidentialScraper:
 
         page = urllib.request.urlopen(link)
         soup = BeautifulSoup(page, "html.parser")
-        evens = soup.select(".even")
-        odds = soup.select(".odd")
-        for i in odds:
-            evens.append(i)
-        for i in evens:
-            g = "https://www.presidency.ucsb.edu" + i.select('a')[1]['href']
-            self.pages.append(urllib.request.urlopen(g))
 
-    def create_corpus(self):
+        pageCount = 0
+        while page is not None:
+            print(f"Processing link #{pageCount}: {link}")
+            pageCount += 1
+            evens = soup.select(".even")
+            odds = soup.select(".odd")
+            for i in odds:
+                evens.append(i)
+            for i in evens:
+                link = "https://www.presidency.ucsb.edu" + i.select('a')[1]['href']
+                self.pages.append(urllib.request.urlopen(link))
+            try:
+                page = urllib.request.urlopen("https://www.presidency.ucsb.edu" +
+                                              soup.find('a', {'title': 'Go to next page'})['href'])
+                soup = BeautifulSoup(page, "html.parser")
+            except TypeError:
+                break
+
+    def create_corpus(self, removeStops=True):
+
+        print(f"create corpus from {len(self.pages)} pages")
         for i in self.pages:
             self.corpus += extract_speech(i)
         self.corpus = self.corpus.lower()
@@ -39,25 +57,58 @@ class PresidentialScraper:
             ']': "",
             "(": "",
             ")": "",
-            ".": "",
+            #".": "",
+            "mr.": "mr",
+            "mrs.": "mrs",
+            "ms.": "ms",
             ",": "",
             "!": "",
             "#": "",
             ":": "",
             "@": "",
             "?": "",
-            "'": "",
+            "they're": "they are",
+            "what's": "what is",
+            "let's": "let us",
+            "there've": "there have",
+            "i've": "i have",
+            "don't": "do not",
+            "we'd": "we had",
+            "he'd": "he had",
+            "we're": "we are",
+            "they're": "they are",
+            "is'nt": "is not",
+            "that's": "that is",
+            "it's": "it is",
+            "he's": "he is",
+            "wasn't": "was not",
+            "i'd": "i would",
+            "you've": "you have",
+            "you're": "you are",
+            "you'd": "you would",
+            "i'm": "i am",
+            "we've": "we have",
+            '-': " ",
             '"': "",
             "&": ""
         }
-        self.corpus = multiple_replace(replace_dict, self.corpus)
-
-        self.corpus = nltk.word_tokenize(self.corpus)
+        # need to remove periods that are not ending a sentence like "Mr. Trump"
         stop = stopwords.words("english")
+        self.corpus = multiple_replace(replace_dict, self.corpus)
+        self.corpusSentenceTokens = [[token for token in nltk.word_tokenize(sent)
+                                      if token not in stop]
+                                     for sent in self.corpus.split('.')]
+        self.corpus = [token for token in nltk.word_tokenize(self.corpus) if token != '.']
+        originalLength = len(self.corpus)
         self.corpus = [word for word in self.corpus if word not in stop]
+        print(f"Removing stop words reduced the {originalLength}-word "
+              f"corpus by {originalLength-len(self.corpus)} to {len(self.corpus)} words.")
         temp = [self.corpus]
         self.corpus = temp
-
+        print(f"corpus of {len(self.corpus[0])} tokenized words.")
+        print(f"corpus of {len(self.corpusSentenceTokens)} tokenized sentences "
+              f"containing {sum(len(sent) for sent in self.corpusSentenceTokens)} words.")
+        return
 
 def extract_speech(page):
     full_text = ""
@@ -77,39 +128,59 @@ def multiple_replace(dict, text):
     # For each match, look-up corresponding value in dictionary
     return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text)
 
+def showSample(vectors, target, count=2, modelLabel=""):
+
+    if target in vectors.key_to_index.keys():
+        sims = vectors.most_similar(target)[:2]
+        if len(sims) >= count:
+            print(f"{count} similar words for '{target}' using the {modelLabel}: {sims}")
+            print(f"with {modelLabel} vocab size {len(vectors.key_to_index)}")
+        else:
+            print(f"{target} did not produce {count} similar words.")
+    else:
+        print(f"{target} is not available in the vocabulary.")
+    return
 
 if __name__ == '__main__':
-    glove_dict = {}
-    with open("Pretrained Vectors/glove.6B.50d.txt", 'r', encoding="utf-8") as file:
-        for i in file:
-            line = i.split()
-            key = line[0]
-            vector = np.array(line[1:], "float32")
-            glove_dict[key] = vector
+    dims = 50 # 300
+    print(f"loading {dims}-dimensional glove vecs")
+    if platform.system() == 'Darwin':
+        fName = f"/Users/lance/Documents/GitHub/DogwhistleProject/Pretrained Vectors/glove.6B.{dims}d.txt"
+    else:
+        fName = f"C:\\Users\\dsm84762\\PycharmProjects\\DogwhistleProject\\Pretrained Vectors\\glove.6B.{dims}d.txt"
+    glove_file = datapath(fName)
+    tmp_file = get_tmpfile("test_word2vec.txt")
+    _ = g2w(glove_file, tmp_file)
 
-    keywords = ["urban", "thug", "drugs", "terror"]
-    glove_vecs = r"Pretrained Vectors/glove.6B.50d.txt"
-    temp = get_tmpfile("test_word2vec.txt")
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    _ = g2w(glove_vecs, temp)
-    glove = KeyedVectors.load_word2vec_format(temp)
+    glove_vectors = KeyedVectors.load_word2vec_format(tmp_file)
+    print(f"Vectors loaded from {glove_file} have {glove_vectors.vector_size} element vectors for "
+          f"{len(glove_vectors.key_to_index)} words.")
+    showSample(glove_vectors, 'president', count=5, modelLabel="glove_vectors")
 
     TrumpList = PresidentialScraper("https://www.presidency.ucsb.edu/advanced-search?field-keywords=&field-keywords2="
-                                    "&field-keywords3=&from%5Bdate%5D=&to%5Bdate%5D=&person2=200301&items_per_page=100")
+                                    "&field-keywords3=&from%5Bdate%5D=&to%5Bdate%5D=&person2=200301&category2%5B0%5D="
+                                    "54&items_per_page=5")
     TrumpList.create_corpus()
-    new_data = TrumpList.corpus
+    cleaned_sentences = TrumpList.corpusSentenceTokens # corpus
 
-    bare = Word2Vec(vector_size=50, min_count=5)
-    print(f"1 bare vocab size: {len(bare.wv.key_to_index.keys())}")
-    print(f"1 'make' is at index {bare.wv.key_to_index.get('make', None)}")
-    bare.build_vocab(new_data)
-    print(f"2 bare vocab size: {len(bare.wv.key_to_index.keys())}")
-    print(f"2 'make' is at index {bare.wv.key_to_index.get('make', None)}")
-    bare.build_vocab([list(glove.key_to_index.keys())], update=True)
-    print(f"3 bare vocab size: {len(bare.wv.key_to_index.keys())}")
-    print(f"3 'make' is at index {bare.wv.key_to_index.get('make', None)}")
-    total = bare.corpus_count
-    bare.train(new_data, total_examples=bare.corpus_count, epochs=bare.epochs)
-    vectors = bare.wv
-    for i in keywords:
-        print(vectors.most_similar(i))
+    base_model = Word2Vec(vector_size=dims, min_count=5)
+    print(f"Build vocabulary for base_model using {sum(len(sent) for sent in cleaned_sentences)} words "
+          f"in {len(cleaned_sentences)} sentences.")
+    base_model.build_vocab(cleaned_sentences)
+
+    showSample(base_model.wv, 'president', count=5, modelLabel="base_model")
+    total_examples = base_model.corpus_count
+
+    base_model.build_vocab([list(glove_vectors.index_to_key)], update=True)
+
+    base_model.train(cleaned_sentences, total_examples=total_examples, epochs=base_model.epochs)
+
+    showSample(base_model.wv, 'president', count=5, modelLabel="base_model retrained with Glove")
+    sims = base_model.wv.most_similar('president')[:2]
+    base_model_wv = base_model.wv
+    print(f"Two similar words for 'president' using the initial base_model retrained: {sims}")
+    print(f"Vectors in base_model have {base_model_wv.vector_size} element vectors for "
+          f"{len(base_model_wv.key_to_index)} words.")
+
+
+    showSample(base_model.wv, 'urban', count=5, modelLabel="base_model retrained with Glove")
